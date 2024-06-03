faa6ae94efece38fe8dbedc3ad7ca46b4176ea68
JamesChaves <javeschames2024@gmail.com>
2024-06-03 11:28:29 -0500

    functional map

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
-console.log("script executed")
+const routeInfo = (route) => {
+    routeObject = {
+        "ecRoute" : "#C40C11",
+        "wcRoute" : "#E6E01C",
+        "nwRoute" : "#BE0BC4",
+        "chRoute" : "#0B6DC4",
+        "txRoute" : "#DEA812",
+        "exRoute" : "#0BC462",
+    }
 
-
-document.getElementById("trigger").addEventListener("click", ()=>{
-    document.getElementById("map").style.display = "flex"
-    document.getElementById("overlay").classList.remove("hide")
-    document.getElementById("trigger").classList.add("hide")
-})
-document.getElementById("backTrigger").addEventListener("click", ()=>{
-    document.getElementById("map").styles.display = "none"
-    document.getElementById("overlay").classList.add("hide")
-    document.getElementById("backTrigger2").addEventListener("click",()=>{
-        document.getElementById("map").classList.remove("hide")
-    document.getElementById("overlay").classList.remove("hide")
-    document.getElementById("trigger").classList.add("hide")
+Array.from(document.querySelectorAll(`.${route}`)).forEach(data => {
+    data.addEventListener("mouseover",()=>{
+        Array.from(document.querySelectorAll(`.${route}`)).forEach(state => state.style.fill = routeObject[`${route}`] )
+    })
+    data.addEventListener("mouseleave",()=>{
+        Array.from(document.querySelectorAll(`.${route}`)).forEach(state => state.style.fill = "#D9D9D9" )
     })
 })
+}
+
+Array.from(document.querySelectorAll("path")).forEach(el => el.addEventListener("mouseenter", ()=>{
+    routeInfo(el.classList.value)
+}))
